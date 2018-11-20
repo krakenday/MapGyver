508a152ebb0a6c89db401104f24c41106b747b00
Alexandre Bourse <alexandre.bourse@gmail.com>
2018-11-20 13:55:31 +0100

    Creation classe metier ElirePhoto+Modification constructeur Jeu+Serializable Jeu

--- mapGyverEJB/ejbModule/business/uc6Jouer/Jeu.java
+++ mapGyverEJB/ejbModule/business/uc6Jouer/Jeu.java
@@ -1,42 +1,56 @@
 package business.uc6Jouer;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Jeu {
+public class Jeu implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	private int id;
 	private String nom;
 	private LocalDate dateCreation;
 	
-	
-	
 	public Jeu() {
-		super();
+		
 	}
 
-
-
-
-
-
-
 	public Jeu(String nom, LocalDate dateCreation) {
-		super();
 		this.nom = nom;
 		this.dateCreation = dateCreation;
 	}
 
-
-
-
-
-
-
 	public Jeu(int id, String nom, LocalDate dateCreation) {
-		super();
 		this.id = id;
 		this.nom = nom;
 		this.dateCreation = dateCreation;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public LocalDate getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(LocalDate dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	@Override
+	public String toString() {
+		return "Jeu [id=" + id + ", nom=" + nom + ", dateCreation=" + dateCreation + "]";
+	}
 }
