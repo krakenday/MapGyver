b084c033a04546965c927cd1dfbb608152968966
Alejandro <al.carrillo@laposte.net>
2018-11-26 16:58:24 +0100

    ajout classes metier UC2

---
+++ mapGyverWeb/src/business/uc2Souvenir/Contenu.java
@@ -0,0 +1,67 @@
+package business.uc2Souvenir;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Alejandro
+ * Un contenu est lié a un point d'interet ou a un voyage
+ *
+ */
+
+public class Contenu extends ArrayList<Souvenir> {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int 			id;
+	private String 			nom;
+	private PointInteret 	pointInteret;
+	private Voyage 			voyage;
+	
+	
+	/**
+	 * Constructeur pour lier un contenu a un point d'interet
+	 * @param id
+	 * @param nom
+	 * @param pointInteret
+	 */
+	public Contenu(int id, String nom, PointInteret pointInteret) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.pointInteret = pointInteret;
+	}
+	
+	
+
+	/**
+	 * Constructeur pour lier un contenu a un Voyage
+	 * @param id
+	 * @param nom
+	 * @param voyage
+	 */
+	public Contenu(int id, String nom, Voyage voyage) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.voyage = voyage;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Contenu [id=" + id + ", nom=" + nom + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
