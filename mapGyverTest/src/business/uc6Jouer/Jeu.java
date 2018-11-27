a9d4de0a7f4f71b4a82cc0712d0c811d330de9f9
Alexandre Bourse <alexandre.bourse@gmail.com>
2018-11-27 12:16:27 +0100

    Ajout de @ManyToOne dans JeuEntity

--- mapGyverTest/src/business/uc6Jouer/Jeu.java
+++ mapGyverTest/src/business/uc6Jouer/Jeu.java
@@ -1,46 +1,29 @@
-package entity.uc6Jouer;
+package business.uc6Jouer;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
 import business.uc8Utilisateur.Utilisateur;
 
-@Entity
-@Table(name = "jeu")
-public abstract class JeuEntity implements Serializable {
+public class Jeu implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	@Column(length = 75)
 	private String nom;
-	@Column(name = "d_crea", nullable = false)
 	private LocalDate dateCreation;
-	@ManyToOne(fetch = FetchType.EAGER)
-	@Column(nullable = false)
 	private Utilisateur utilisateur;
 
-	public JeuEntity() {
+	public Jeu() {
 
 	}
 
-	public JeuEntity(String nom, LocalDate dateCreation, Utilisateur utilisateur) {
+	public Jeu(String nom, LocalDate dateCreation, Utilisateur utilisateur) {
 		this.nom = nom;
 		this.dateCreation = dateCreation;
 		this.utilisateur = utilisateur;
 	}
 
-	public JeuEntity(int id, String nom, LocalDate dateCreation, Utilisateur utilisateur) {
+	public Jeu(int id, String nom, LocalDate dateCreation, Utilisateur utilisateur) {
 		this.id = id;
 		this.nom = nom;
 		this.dateCreation = dateCreation;
@@ -81,14 +64,7 @@ public abstract class JeuEntity implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Jeu [id=" + id + ", nom=" + nom + ", dateCreation=" + dateCreation + "Utilisateur Id "
+		return "Jeu [id=" + id + ", nom=" + nom + ", dateCreation=" + dateCreation + "Utilisateur Id"
 				+ utilisateur.getId() + "]";
 	}
-
-	/**
-	 * Va permettre de fournir le Metier lier a l'entity
-	 * 
-	 * @return
-	 */
-	public abstract Class<?> getMappingMetier();
 }
